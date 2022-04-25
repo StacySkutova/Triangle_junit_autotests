e9bde1964673db0d305631c776007eeb0d7b52f2
StacySkutova <skutova2.20@gmail.com>
2022-04-25 23:22:20 +0300

    fix: updated with params

--- src/main/java/anastasiya_skutova/Triangle.java
+++ src/main/java/anastasiya_skutova/Triangle.java
@@ -8,28 +8,27 @@ public class Triangle {
     private static final String TRIANGLE_EQUILATERAL = "Треугольник равносторонний.";
     private static final String TRIANGLE_OTHER = "Треугольник ни равнобедренный, ни равносторонний.";
 
-
-    public String TriangleNoNegativeAndZeroNumbers(int a, int b, int c) {
-        if (a < 0 || b < 0 || c < 0 || a == 0 || b == 0 || c == 0) {
+    public String TriangleNoNegativeAndZeroNumbers(int firstSide, int secondSide, int thirdSide) {
+        if (firstSide < 0 || secondSide < 0 || thirdSide < 0 || firstSide == 0 || secondSide == 0 || thirdSide == 0) {
             return NO_NEGATIVE_ZERO_NUMBERS;
         } else {
             return null;
         }
     }
 
-    public String TriangleExists(int a, int b, int c) {
-        if (a < b + c && b < a + c && c < a + b && a != b + c && b != a + c && c != a + b) {
+    public String TriangleExists(int firstSide, int secondSide, int thirdSide) {
+        if (firstSide < secondSide + thirdSide && secondSide < firstSide + thirdSide && thirdSide < firstSide + secondSide && firstSide != secondSide + thirdSide && secondSide != firstSide + thirdSide && thirdSide != firstSide + secondSide) {
             return TRIANGLE_EXISTS;
         } else {
             return TRIANGLE_NOT_EXISTS;
         }
     }
 
-    public String TriangleType(int a, int b, int c) {
-        if (a < b + c && b < a + c && c < a + b && a != b + c && b != a + c && c != a + b) {
-            if ((a == b && c != a) || (a == c && b != a) || (b == c && a != b)) {
+    public String TriangleType(int firstSide, int secondSide, int thirdSide) {
+        if (firstSide < secondSide + thirdSide && secondSide < firstSide + thirdSide && thirdSide < firstSide + secondSide && firstSide != secondSide + thirdSide && secondSide != firstSide + thirdSide && thirdSide != firstSide + secondSide) {
+            if ((firstSide == secondSide && thirdSide != firstSide) || (firstSide == thirdSide && secondSide != firstSide) || (secondSide == thirdSide && firstSide != secondSide)) {
                 return TRIANGLE_ISOSCELES;
-            } else if (a == b && b == c && a == c) {
+            } else if (firstSide == secondSide && secondSide == thirdSide && firstSide == thirdSide) {
                 return TRIANGLE_EQUILATERAL;
             } else {
                 return TRIANGLE_OTHER;
--- src/test/java/anastasiya_skutova/TriangleTest.java
+++ src/test/java/anastasiya_skutova/TriangleTest.java
@@ -1,15 +1,92 @@
 package anastasiya_skutova;
 
+import org.junit.Assume;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 import static org.junit.Assert.*;
 
-
+@RunWith(value = Parameterized.class)
 public class TriangleTest {
+    private Type type;
+    private int firstSide;
+    private int secondSide;
+    private int thirdSide;
+    private String expectedResult;
+    private Triangle triangle;
+
+    public TriangleTest(Type type, int firstSide, int secondSide, int thirdSide, String expectedResult) {
+        this.type = type;
+        this.firstSide = firstSide;
+        this.secondSide = secondSide;
+        this.thirdSide = thirdSide;
+        this.expectedResult = expectedResult;
+    }
+
+    @Before
+    public void setUp() {
+        triangle = new Triangle();
+    }
+
+    enum Type {NO_NEGATIVE_AND_ZERO, EXISTENCE, TYPE}
+
+    ;
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {Type.NO_NEGATIVE_AND_ZERO, 0, 1, 1, "Ввод отрицательных чисел или 0 для определения значения стороны треугольника запрещен! Попробуйте снова, учитывая условия."},
+                {Type.NO_NEGATIVE_AND_ZERO, 1, 0, 1, "Ввод отрицательных чисел или 0 для определения значения стороны треугольника запрещен! Попробуйте снова, учитывая условия."},
+                {Type.NO_NEGATIVE_AND_ZERO, 1, 1, 0, "Ввод отрицательных чисел или 0 для определения значения стороны треугольника запрещен! Попробуйте снова, учитывая условия."},
+                {Type.NO_NEGATIVE_AND_ZERO, -1, 1, 1, "Ввод отрицательных чисел или 0 для определения значения стороны треугольника запрещен! Попробуйте снова, учитывая условия."},
+                {Type.NO_NEGATIVE_AND_ZERO, 1, -1, 1, "Ввод отрицательных чисел или 0 для определения значения стороны треугольника запрещен! Попробуйте снова, учитывая условия."},
+                {Type.NO_NEGATIVE_AND_ZERO, 1, 1, -1, "Ввод отрицательных чисел или 0 для определения значения стороны треугольника запрещен! Попробуйте снова, учитывая условия."},
+                {Type.EXISTENCE, 8, 5, 4, "Треугольник существует."},
+                {Type.EXISTENCE, 10000, 15000, 17000, "Треугольник существует."},
+                {Type.EXISTENCE, 999999999, 777777777, 1000000000, "Треугольник существует."},
+                {Type.EXISTENCE, 1, 2, 3, "Треугольник не существует."},
+                {Type.EXISTENCE, 10000, 20000, 10000, "Треугольник не существует."},
+                {Type.EXISTENCE, 100000000, 999999999, 1, "Треугольник не существует."},
+                {Type.TYPE, 2, 2, 3, "Треугольник равнобедренный."},
+                {Type.TYPE, 10000, 15000, 10000, "Треугольник равнобедренный."},
+                {Type.TYPE, 999999999, 1000000000, 1000000000, "Треугольник равнобедренный."},
+                {Type.TYPE, 7, 7, 7, "Треугольник равносторонний."},
+                {Type.TYPE, 20000, 20000, 20000, "Треугольник равносторонний."},
+                {Type.TYPE, 1000000000, 1000000000, 1000000000, "Треугольник равносторонний."},
+                {Type.TYPE, 3, 5, 7, "Треугольник ни равнобедренный, ни равносторонний."},
+                {Type.TYPE, 10000, 12000, 20000, "Треугольник ни равнобедренный, ни равносторонний."},
+                {Type.TYPE, 1000000000, 888888888, 555555555, "Треугольник ни равнобедренный, ни равносторонний."},
+        });
+    }
+
 
     @Test
     public void triangleNoNegativeAndZeroNumbers() {
-        Triangle triangle = new Triangle();
+        Assume.assumeTrue(type == Type.NO_NEGATIVE_AND_ZERO);
+        assertEquals(expectedResult, triangle.TriangleNoNegativeAndZeroNumbers(firstSide, secondSide, thirdSide));
+    }
+
+    @Test
+    public void triangleExists() {
+        Assume.assumeTrue(type == Type.EXISTENCE);
+        assertEquals(expectedResult, triangle.TriangleExists(firstSide, secondSide, thirdSide));
+    }
+
+    @Test
+    public void triangleType() {
+        Assume.assumeTrue(type == Type.TYPE);
+        assertEquals(expectedResult, triangle.TriangleType(firstSide, secondSide, thirdSide));
+    }
+
+
+
+    /*@Test
+    public void triangleNoNegativeAndZeroNumbers() {
         assertEquals("Ввод отрицательных чисел или 0 для определения значения стороны треугольника запрещен! Попробуйте снова, учитывая условия.", triangle.TriangleNoNegativeAndZeroNumbers(0, 1, 1));
         assertEquals("Ввод отрицательных чисел или 0 для определения значения стороны треугольника запрещен! Попробуйте снова, учитывая условия.", triangle.TriangleNoNegativeAndZeroNumbers(1, 0, 1));
         assertEquals("Ввод отрицательных чисел или 0 для определения значения стороны треугольника запрещен! Попробуйте снова, учитывая условия.", triangle.TriangleNoNegativeAndZeroNumbers(1, 1, 0));
@@ -41,5 +118,5 @@ public class TriangleTest {
         assertEquals("Треугольник ни равнобедренный, ни равносторонний.", triangle.TriangleType(3, 5, 7));
         assertEquals("Треугольник ни равнобедренный, ни равносторонний.", triangle.TriangleType(10000, 12000, 20000));
         assertEquals("Треугольник ни равнобедренный, ни равносторонний.", triangle.TriangleType(1000000000, 888888888, 555555555));
-    }
+    }*/
 }
